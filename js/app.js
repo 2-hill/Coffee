b0155e91c24fcd84fa151769666185805bffcd7b
2hill <jamil.rahou@laposte.net>
2018-02-24 18:09:44 +0100

    :coffee: is ready

---
+++ js/app.js
@@ -0,0 +1,75 @@
+"use strict";
+
+// fetching json data
+fetch("./json/data.js").then(function(response) {
+  if (response.status !== 200) {
+    console.log('Looks like there was a problem. Status Code: ' + response.status);
+    return;
+  }
+}).catch(function(err) {
+  console.log('Fetch Error :-S', err);
+});
+
+let content = TABLE_DATA.length;
+let body = document.getElementsByTagName('tbody')[0];
+// loop over json to populate table
+let populate = function() {
+  for (let i = 0; i < content; i++) {
+    // append rows
+    let tr = document.createElement('tr');
+    body.appendChild(tr);
+    // filling name cells
+    let tdname = document.createElement('td');
+    tdname.innerHTML = TABLE_DATA[i].name;
+    tr.appendChild(tdname);
+    // filling image cells
+    let img = document.createElement('img')
+    img.setAttribute('src', TABLE_DATA[i].thumbnailUrl)
+    tr.appendChild(img);
+    // filling composition cells
+    let tdid = document.createElement('td');
+    tdid.innerHTML = TABLE_DATA[i].composition;
+    tr.appendChild(tdid);
+    // filling price cells
+    let tdprice = document.createElement('td');
+    tdprice.innerHTML = TABLE_DATA[i].price;
+    tr.appendChild(tdprice);
+  };
+};
+populate();
+
+// shuffle rows function
+let randomize = function() {
+  let body = document.getElementsByTagName('tbody')[0];
+  for (let i = body.children.length; i >= 0; i--) {
+    body.appendChild(body.children[Math.random() * i | 0]);
+  }
+};
+// set interval function
+let intervalID;
+let everySecond = function() {
+  return intervalID = setInterval(randomize, 1000);
+};
+
+// stop interval function
+let stopEverySecond = function() {
+  return clearInterval(intervalID)
+};
+
+// sort function
+// reste a injecter dans le dom
+
+let descending = () => [...TABLE_DATA.sort((a, b) => a.price > b.price ? -1 : 1 )];
+let ascending = () => [...TABLE_DATA.sort((a, b) => a.price > b.price ? 1 : -1 )];
+//console.log(descending);
+//console.log(ascending);
+
+descending();
+
+
+
+  // listen randomize, stop randomize and sorting events
+  let clickStart = document.getElementById('start').addEventListener("click", everySecond);
+  let clickStop = document.getElementById('stop').addEventListener("click", stopEverySecond);
+  let clickSortAsc = document.getElementById('sort').addEventListener("click", ascending);
+  let clickSortDesc = document.getElementById('sort').addEventListener("click", descending)
---
+++ json/data.js
@@ -0,0 +1,62 @@
+let TABLE_DATA = [
+  {
+    "composition": "We take Frappuccino® roast coffee and vanilla bean powder, combine them with milk and ice, then top it with whipped cream. Tastes like happiness.",
+    "name": "Caffè Vanilla",
+    "thumbnailUrl": "image/1.jpg",
+    "price": '$' + 8
+  },
+  {
+    "composition": "Buttery caramel syrup meets coffee, milk and ice for a rendezvous in the blender. Then whipped cream and caramel sauce layer the love on top.",
+    "name": "Caramel",
+    "thumbnailUrl": "image/2.jpg",
+    "price": '$' + 9
+  },
+  {
+    "composition": "Buttery caramel syrup is combined with Frappuccino® roast coffee and nonfat milk, then blended with ice. Sip up the same awesome taste, just with fewer calories.",
+    "name": "Caramel Light",
+    "thumbnailUrl": "image/3.jpg",
+    "price": '$' + 9
+  },
+  {
+    "composition": "Coffee meets milk and ice in a blender for a rumble and tumble and together they create one of our original Frappuccino® beverages.",
+    "name": "Coffee",
+    "thumbnailUrl": "image/4.jpg",
+    "price": '$' + 7
+  },
+  {
+    "composition": "Start with the Vanilla Bean Crème Frappuccino®, add hazelnut syrup, blend together and finish with whipped cream. A sip of cake flavor in every bite.",
+    "name": "Cupcake Crème",
+    "thumbnailUrl": "image/5.jpg",
+    "price": '$' + 10
+  },
+  {
+    "composition": "Rich mocha-flavored sauce meets up with chocolaty chips, milk and ice for a blender bash. Top it off with sweetened whipped cream and mocha drizzle for a real party in your mouth.",
+    "name": "Double Chocolaty Chip Crème",
+    "thumbnailUrl": "image/6.jpg",
+    "price": '$' + 12
+  },
+  {
+    "composition": "Our take on Horchata is a creamy, cool, cinnamon-sweet Frappuccino drink blended to perfection. Delightful during the warmer months, and like summer, not here for long.",
+    "name": "Horchata Almondmilk",
+    "thumbnailUrl": "image/7.jpg",
+    "price": '$' + 11
+  },
+  {
+    "composition": "Mocha sauce, Frappuccino® chips are blended with coffee and nonfat milk and ice. It's a lighter chip off the original java block",
+    "name": "Java Chip Light",
+    "thumbnailUrl": "image/8.jpg",
+    "price": '$' + 10
+  },
+  {
+    "composition": "A confectionery-inspired blend of chocolaty chips, mocha sauce, raspberry and vanilla syrup, topped with whipped cream. Perfectly sweet and velvety smooth.",
+    "name": "Red Velvet Cake Crème",
+    "thumbnailUrl": "image/9.jpg",
+    "price": '$' + 14
+  },
+  {
+    "composition": "Strawberries and milk are blended with ice and topped with a swirl of whipped cream. Sip on the crème of the crop.",
+    "name": "Strawberries & Crème",
+    "thumbnailUrl": "image/10.jpg",
+    "price": '$' + 9
+  }
+]
