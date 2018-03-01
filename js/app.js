a2294bf9c89198c7a6f1d057a5dc82efc44d668a
2hill <jamil.rahou@laposte.net>
2018-03-01 21:36:55 +0100

    sort functions added :construction_worker:

--- js/app.js
+++ js/app.js
@@ -1,39 +1,44 @@
 "use strict";
-
 // fetching json data
-fetch("./json/data.js").then(function(response) {
-  if (response.status !== 200) {
-    console.log('Looks like there was a problem. Status Code: ' + response.status);
-    return;
-  }
-}).catch(function(err) {
-  console.log('Fetch Error :-S', err);
-});
+fetch("./json/data.js")
+  .then(function(response) {
+        if (response.status !== 200) {
+          console.log('Looks like there was a problem. Status Code: ' + response.status);
+          return;
+        }})
+  .catch(function(err) {console.log('Fetch Error :-S', err)});
 
-let content = TABLE_DATA.length;
-let body = document.getElementsByTagName('tbody')[0];
 // loop over json to populate table
 let populate = function() {
+  let tr;
+  let tdPrice;
+  let content = TABLE_DATA.length;
+  let body = document.getElementsByTagName('tbody')[0];
+
   for (let i = 0; i < content; i++) {
-    // append rows
-    let tr = document.createElement('tr');
-    body.appendChild(tr);
-    // filling name cells
-    let tdname = document.createElement('td');
-    tdname.innerHTML = TABLE_DATA[i].name;
-    tr.appendChild(tdname);
-    // filling image cells
-    let img = document.createElement('img')
-    img.setAttribute('src', TABLE_DATA[i].thumbnailUrl)
-    tr.appendChild(img);
-    // filling composition cells
-    let tdid = document.createElement('td');
-    tdid.innerHTML = TABLE_DATA[i].composition;
-    tr.appendChild(tdid);
-    // filling price cells
-    let tdprice = document.createElement('td');
-    tdprice.innerHTML = TABLE_DATA[i].price;
-    tr.appendChild(tdprice);
+      // append rows
+      tr = document.createElement('tr');
+      body.appendChild(tr);
+      // filling name cells
+      let tdFlavor = document.createElement('td');
+      tdFlavor.setAttribute('id', "flavorColumn")
+      tdFlavor.innerHTML = TABLE_DATA[i].flavor;
+      tr.appendChild(tdFlavor);
+      // filling image cells
+      let img = document.createElement('img')
+      img.setAttribute('src', TABLE_DATA[i].thumbnailUrl)
+      img.setAttribute('id', "imgColumn")
+      tr.appendChild(img);
+      // filling composition cells
+      let tdCompo = document.createElement('td');
+      tdCompo.setAttribute('id', "compoColumn")
+      tdCompo.innerHTML = TABLE_DATA[i].composition;
+      tr.appendChild(tdCompo);
+      // filling price cells
+      tdPrice = document.createElement('td');
+      tdPrice.setAttribute("id", "priceColumn");
+      tdPrice.innerHTML = "$" + TABLE_DATA[i].price;
+      tr.appendChild(tdPrice);
   };
 };
 populate();
@@ -51,27 +56,67 @@ let everySecond = function() {
   intervalID = setInterval(randomize, 1000)
   document.getElementById('start').disabled = true;
 };
-
 // stop interval function
 let stopEverySecond = function() {
   clearInterval(intervalID)
   document.getElementById('start').disabled = false;
 };
 
-// sort function
-// reste a injecter dans le dom
+// sort functions
+let descending = function () {
+let descArray = [...TABLE_DATA.sort( (a, b) => a.price > b.price ? -1 : 1)];
 
-let descending = () => [...TABLE_DATA.sort((a, b) => a.price > b.price ? -1 : 1 )];
-let ascending = () => [...TABLE_DATA.sort((a, b) => a.price > b.price ? 1 : -1 )];
-//console.log(descending);
-//console.log(ascending);
+    for (let i = 0; i < descArray.length; i++) {
+      let oldPrice = document.getElementById('priceColumn');
+      let newPrice = document.createElement('td');
+      newPrice.innerHTML = "$" + descArray[i].price;
+      oldPrice.parentNode.replaceChild(newPrice, oldPrice);
 
-descending();
+      let oldImg = document.getElementById("imgColumn")
+      let newImg = document.createElement('img');
+      newImg.setAttribute('src', descArray[i].thumbnailUrl)
+      oldImg.parentNode.replaceChild(newImg, oldImg);
 
+      let oldFlavor = document.getElementById('flavorColumn');
+      let newFlavor = document.createElement('td');
+      newFlavor.innerHTML = descArray[i].flavor;
+      oldFlavor.parentNode.replaceChild(newFlavor, oldFlavor);
 
+      let oldCompo = document.getElementById('compoColumn');
+      let newCompo = document.createElement('td');
+      newCompo.innerHTML = descArray[i].composition;
+      oldCompo.parentNode.replaceChild(newCompo, oldCompo);
+  };
+//document.getElementById('sortDesc').disabled = true;
+};
 
-  // listen randomize, stop randomize and sorting events
-  let clickStart = document.getElementById('start').addEventListener("click", everySecond);
-  let clickStop = document.getElementById('stop').addEventListener("click", stopEverySecond);
-  let clickSortAsc = document.getElementById('sort').addEventListener("click", ascending);
-  let clickSortDesc = document.getElementById('sort').addEventListener("click", descending)
+let ascending = function () {
+let ascArray = [...TABLE_DATA.sort((a, b) => a.price > b.price ? 1 : -1 )];
+    for (let i = 0; i < ascArray.length; i++) {
+      let oldPrice = document.getElementById('priceColumn');
+      let newPrice = document.createElement('td');
+      newPrice.innerHTML = "$" + ascArray[i].price;
+      oldPrice.parentNode.replaceChild(newPrice, oldPrice);
+
+      let oldImg = document.getElementById("imgColumn")
+      let newImg = document.createElement('img');
+      newImg.setAttribute('src', ascArray[i].thumbnailUrl)
+      oldImg.parentNode.replaceChild(newImg, oldImg);
+
+      let oldFlavor = document.getElementById('flavorColumn');
+      let newFlavor = document.createElement('td');
+      newFlavor.innerHTML = ascArray[i].flavor;
+      oldFlavor.parentNode.replaceChild(newFlavor, oldFlavor);
+
+      let oldCompo = document.getElementById('compoColumn');
+      let newCompo = document.createElement('td');
+      newCompo.innerHTML = ascArray[i].composition;
+      oldCompo.parentNode.replaceChild(newCompo, oldCompo);
+    }
+};
+
+// listen randomize, stop randomize and sorting events
+let clickStart = document.getElementById('start').addEventListener("click", everySecond);
+let clickStop = document.getElementById('stop').addEventListener("click", stopEverySecond);
+let clickSortDesc = document.getElementById('sortDesc').addEventListener("click", descending)
+let clickSortAsc = document.getElementById('sortAsc').addEventListener("click", ascending);
